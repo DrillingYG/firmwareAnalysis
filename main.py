686ec1930a3820ed9a00c9bfe7c326d85a73eefd
ChangGwon Kim <empty7792@gmail.com>
2019-11-14 22:48:21 +0900

    test

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import os
 
 if __name__ == '__main__':
-    root = "Y:\\내 드라이브\\Lab\\Network Storage Artifact\\실험\\Firmware Analysis\\volume-0"
+
+    root = "G:\\내 드라이브\\Lab\\Network Storage Artifact\\실험\\Firmware Analysis\\volume-0"
     dir_list = os.listdir(root)
 
-    for dir in dir_list:
-        print("dir : ", dir)
-        dir_path = os.path.join(root, dir)
+    for dirname in dir_list:
+        print("dir : ", dirname)
+        dir_path = os.path.join(root, dirname)
         if os.path.isdir(dir_path):
             file_list = os.listdir(dir_path)
             for file in file_list:
-                if(file.split('.')[-1] == "nvar"):
+                if file.split('.')[-1] == 'nvar':
                     print(file)
     

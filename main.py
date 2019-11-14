6d08267de6bb957d2282f4ca7c9e4167fa68bcb1
ChangGwon Kim <empty7792@gmail.com>
2019-11-14 23:20:14 +0900

    modifing test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 if __name__ == '__main__':
-    root = "Y:\\내 드라이브\\Lab\\Network Storage Artifact\\실험\\Firmware Analysis\\volume-0"
+    root = "G:\\내 드라이브\\Lab\\Network Storage Artifact\\실험\\Firmware Analysis\\volume-0"
     dir_list = os.listdir(root)
 
     for dirname in dir_list:
